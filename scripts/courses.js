a0b77232bcc66a2d4cc3d7c5ee0015e1bdc9b7e6
godfreygova <ggova@byupathway.edu>
2026-01-29 12:40:14 +0200

    commit

--- scripts/courses.js
+++ scripts/courses.js
@@ -65,3 +65,22 @@ document.addEventListener('DOMContentLoaded', () => {
     renderCourses(courses);
     updateCreditTotal(courses);
 });
+
+
+function displayCourseDetails(course) {
+    courseDetails.innerHTML = '';
+    courseDetails.innerHTML = `
+    <button id="closeModal">❌</button>
+    <h2>${course.subject} ${course.number}</h2>
+    <h3>${course.title}</h3>
+    <p><strong>Credits</strong>: ${course.credits}</p>
+    <p><strong>Certificate</strong>: ${course.certificate}</p>
+    <p>${course.description}</p>
+    <p><strong>Technologies</strong>: ${course.technology.join(', ')}</p>
+  `;
+    courseDetails.showModal();
+
+    closeModal.addEventListener("click", () => {
+        courseDetails.close();
+    });
+}
